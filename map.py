87d14fd5d337efc25bf7ec6ddea8117026d5bdaf
CoderSherlock <haopengzhan@gmail.com>
2016-12-21 00:41:21 -0500

    Add super blocks support, fix some format

--- map.py
+++ map.py
@@ -1,7 +1,18 @@
+# -*- coding: UTF-8 -*-
 import random
 
 
 class block:
+
+    #################################################################
+    # Initialize                                                    #
+    # btype:    block type = 'normal' OR 'super' OR 'jail'(TODO)    #
+    # bvalue:   block value                                         #
+    # bincome:  block income                                        #
+    # bnext:    next block                                          #
+    # blast:    last block                                          #
+    # bname:    block name                                          #        
+    #################################################################
 
     def __init__(self, btype, bvalue, bincome, bnext, blast, bname):
         self.btype = btype
@@ -12,32 +23,101 @@ class block:
         self.owner = -1
         self.bname = bname
 
+        self.trigger = None
+
+    #################
+    # toString      #
+    #################
+
     def __str__(self):
-        return 'Blocks:{0}, ({1},{2})\tOwner:{3};\t next:{4},prev:{5}'.format(
-            self.btype, self.bvalue, self.bincome, self.owner, self.bnext.bname,
-            self.blast.bname)
+        return '{6:<12}\t[{0}] ({1:<5},{2:<5})\tOwner:{3};\t next:{4},prev:{5}'.format(
+            self.btype[0], self.bvalue, self.bincome, self.owner, self.bnext.bname,
+            self.blast.bname, self.bname)
 
 
 class district:
 
-    def __init__(self, msize):
-        self.msize = msize
+    #########################################################
+    # Initialize                                            #
+    # msize:    Map size = small OR medium OR huge OR crazy #
+    # mseed:    Map generation seed                         #
+    #########################################################
+
+    def __init__(self, msize, mseed=1):
+        self.msize = sizeTable[msize]
+        self.mseed = mseed
         self.blocks = []
-        for i in range(0, sizeTable[self.msize]):
-            tmpB = block('normal', random.randint(30, 60) * 10, random.randint(30, 60) * 1000,
-                         None, None, i)
+        self.generate_map()
+
+    #########################################
+    # generate a new map                    #
+    # TODO: adding different types of map   #
+    #########################################
+
+    def generate_map(self):
+        
+        # configuration
+        value_base = 30
+        value_head = 60
+        income_base = 30
+        income_head = 60
+        multiple = 10
+        sumper_multiple = 5
+
+        # Add blocks
+        prevalue = -1
+        preincome = -1
+
+        namepool = blockname[:]
+        for i in range(0, self.msize):
+
+            # super blocks
+            if random.randint(1, 100) >=80 :
+                name = blockname.pop(random.randint(0, len(blockname)-1))
+                value = random.randint(value_base, value_head) * multiple * sumper_multiple
+                income = random.randint(income_base, income_head) * multiple * sumper_multiple 
+                tmpB = block('super', value, income, None, None, name)
+                prevalue = preincome = -1
+
+            # normal blocks
+            else: 
+                name = blockname.pop(random.randint(0, len(blockname)-1))
+                if prevalue == -1:
+                    value = random.randint(value_base, value_head) * multiple
+                    income = random.randint(income_base, income_head) * multiple 
+                    tmpB = block('normal', value, income, None, None, name)
+                    prevalue = value 
+                    preincome = income
+                else:
+                    if random.randint(0, 10) >= 8:
+                        value = random.randint(value_base, value_head) * multiple
+                        income = random.randint(income_base, income_head) * multiple 
+                        tmpB = block('normal', value, income, None, None, name)
+                        prevalue = value 
+                        preincome = income
+                    else:
+                        tmpB = block('normal', prevalue, preincome, None, None, name)
+
             self.blocks.append(tmpB)
         for i in range(0, len(self.blocks)):
             self.blocks[i].blast = self.blocks[i - 1]
             self.blocks[i - 1].bnext = self.blocks[i]
-        self.msize = sizeTable[self.msize]
+
 
     def display(self):
         for i in self.blocks:
             print(i)
 
-sizeTable = {'small': 10, 'medium': 20, 'large': 30, 'crazy': 150}
+sizeTable = {'small': 30, 'medium': 50, 'large': 100, 'crazy': 150}
 
-# if __name__ == '__main__':
-#     m = district('small')
-#     m.display()
+blockname = ['北京', '上海', '广州','天津','重庆','石家庄','太原','西安','沈阳','长春','哈尔滨',\
+        '济南','南京','杭州','福州','南宁','南昌','长沙','武汉','郑州','银川','西宁','兰州',\
+        '拉萨','昆明','成都','贵阳','乌鲁木齐','呼和浩特','合肥','深圳','青岛','苏州','大连',\
+        '宁波','无锡','厦门','常州','东莞','温州','佛山','海口','台北','湖州','唐山','临沂',\
+        '嘉兴','绍兴','南通','徐州','泉州','烟台','潍坊','珠海','洛阳','中山','金华','淮安',\
+        '威海','淄博','扬州','芜湖','盐城','宜昌','襄阳','绵阳','新竹','高雄','保定','延安',\
+        '大同','大理','日喀则','喀什','桂林','齐齐哈尔','三亚','香港','澳门','新加坡','平壤','首尔','东京']
+
+if __name__ == '__main__':
+    m = district('medium')
+    m.display()
