122ff2723eca5a571a45daaf39d1db61bc3f9351
CoderSherlock <haopengzhan@gmail.com>
2016-12-22 22:23:14 -0500

    Add hos/jail blocks, re-organized files

--- map.py
+++ map.py
@@ -20,23 +20,39 @@ class block:
         self.bincome = bincome
         self.bnext = bnext
         self.blast = blast
-        self.owner = -1
+        self.owner = 'Tom'
         self.bname = bname
         self.players = []
+        self.level = 0
+
 
         self.trigger = None
+        self.display_type = 0
 
     #################
     # toString      #
     #################
 
     def __str__(self):
-        return '{6:<12}\t[{0}] ({1:<5},{2:<5})\tOwner:{3};\t next:{4},prev:{5}'.format(
-            self.btype[0], self.bvalue, self.bincome, self.owner, self.bnext.bname,
-            self.blast.bname, self.bname)
-
-
-class district:
+        if self.display_type == 0:
+            return '{6:<12}\t[{0}] ({1:<5},{2:<5})\tOwner:{3};\t next:{4},prev:{5}'.format(
+                self.btype[0], self.bvalue, self.bincome, self.owner, self.bnext.bname, self.blast.bname, self.bname)
+        else:
+            # Size: 24 * 2  
+            # #-----------------------#
+            # | Name________ Owner_   |
+            # | [T] Lv.3  99999/99999 |
+            # #-----------------------#
+            return ' {0:<12}  {1:>8} \n [{2:1}] {3:3}{4:<2}  {5:<5}{6}{7:>5} '.format(
+                    self.bname,\
+                    self.owner if self.bvalue!=-1 else '',\
+                    self.btype[0].upper(),\
+                    'Lv.' if self.bvalue!=-1 else '   ',\
+                    self.level if self.bvalue!=-1 else '',\
+                    self.bvalue if self.bvalue!=-1 else '',\
+                    '/' if self.bvalue!=-1 else ' ',\
+                    self.bincome if self.bvalue!=-1 else '')
+class district: 
 
     #########################################################
     # Initialize                                            #
@@ -45,9 +61,19 @@ class district:
     #########################################################
 
     def __init__(self, msize, mseed=1):
+        
+        # Basic Variables
         self.msize = sizeTable[msize]
-        self.mseed = mseed
         self.blocks = []
+
+        # Mid-Level Variables
+        self.mseed = mseed
+
+        self.hospital_loc = -1
+        self.jail_loc = -1
+        
+
+        # Preparation Method 
         self.generate_map()
 
     #########################################
@@ -70,6 +96,8 @@ class district:
         preincome = -1
 
         namepool = blockname[:]
+        
+        # Generate normal and super blocks
         for i in range(0, self.msize):
 
             # super blocks
@@ -100,6 +128,16 @@ class district:
                         tmpB = block('normal', prevalue, preincome, None, None, name)
 
             self.blocks.append(tmpB)
+
+        # Genertae Only-one Special blocks
+        # Hospital:
+        self.hospital_loc = random.randint(0, self.msize-1)
+        self.blocks[self.hospital_loc] = block('hospital', -1, -1, None, None, 'Hospital')
+        # Jail
+        skew_value = random.randint(-5, 5)
+        self.jail_loc = (self.hospital_loc + self.msize/2) % self.msize + skew_value
+        self.blocks[self.jail_loc] = block('jail', -1, -1, None, None, 'Jail')
+        
         for i in range(0, len(self.blocks)):
             self.blocks[i].blast = self.blocks[i - 1]
             self.blocks[i - 1].bnext = self.blocks[i]
@@ -118,7 +156,7 @@ class district:
             	tmp = tmp % self.msize
 
             if self.blocks[tmp].btype == 'bomb':
-            	tmp = 0	#TODO: hospital loc
+            	tmp = self.hospital_loc
                 break
 
         location = tmp
@@ -127,6 +165,8 @@ class district:
 
     def display(self):
         for i in self.blocks:
+            i.display_type = 1
+            print '------------------------'
             print(i)
 
 sizeTable = {'small': 30, 'medium': 50, 'large': 100, 'crazy': 150}
@@ -137,8 +177,8 @@ blockname = ['北京', '上海', '广州','天津','重庆','石家庄','太原'
         '宁波','无锡','厦门','常州','东莞','温州','佛山','海口','台北','湖州','唐山','临沂',\
         '嘉兴','绍兴','南通','徐州','泉州','烟台','潍坊','珠海','洛阳','中山','金华','淮安',\
         '威海','淄博','扬州','芜湖','盐城','宜昌','襄阳','绵阳','新竹','高雄','保定','延安',\
-        '大同','大理','日喀则','喀什','桂林','齐齐哈尔','三亚','香港','澳门','新加坡','平壤','首尔','东京']
+        '大同','大理','日喀则','喀什','桂林','齐齐哈尔','三亚','香港','澳门','Singapore','平壤','Seoul','Tokyo']
 
 if __name__ == '__main__':
-    m = district('medium')
+    m = district('small')
     m.display()
